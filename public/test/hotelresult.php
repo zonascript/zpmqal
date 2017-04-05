ee6be5da4d614a8ea630e9daa0351c916b4f0c50
Ajay Kumar <ajay@flygoldfinch.com>
2017-04-06 02:23:32 +0530

    deleted some unused or junk file

--- public/test/hotelresult.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$recoveredData = file_get_contents('hotelresult.json');
-$recoveredArrays = json_decode($recoveredData, true);
-echo '<pre>'; print_r($recoveredArrays); echo '</pre>'; 
-
-?>
