799be9db71a3b79cc948d33cee278333a95480f9
Ajay Kumar <ajay@flygoldfinch.com>
2017-04-23 02:37:29 +0530

    added route

--- routes/b2b.php
+++ routes/b2b.php
@@ -5,7 +5,7 @@ Route::get('', 'B2bApp\PagesController@getIndex');
 
 // This for package html
 Route::get('your/package/detail/{hashId}', 'B2bApp\PdfHtmlController@htmlByHashIdWithTrack');
-
+Route::get('a/l/htdetail', 'HotelApp\AgodaHotelsController@loopHotelDetails');
 
 Route::group(['middleware' => ['auth']], function(){
 
