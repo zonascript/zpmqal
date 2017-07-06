3ccb80cbc0533ff701c268231d05156a253ee21b
Ajay Kumar <ajay@flygoldfinch.com>
2017-07-06 19:34:27 +0530

    improved

--- resources/views/subway/pages/home_partials/slideshow.blade.php
+++ resources/views/subway/pages/home_partials/slideshow.blade.php
@@ -1,23 +1,23 @@
+<?php 
+	$tempImages = [
+			urlImage("images/country/australia.jpg"),
+			urlImage("images/country/european.jpg"),
+			urlImage("images/country/fiji.jpg"),
+			urlImage("images/country/singapore.jpg"),
+			urlImage("images/country/south-africa.jpg"),
+		];
+?>
+
 <div class="col-md-12 col-sm-12 col-xs-12">
 	<div class="row">
 		<div id="ninja-slider">
 			<div class="slider-inner">
 				<ul class="nomargin">
-					<li>
-						<a class="ns-img" href="{{url('/')}}/test/slideshow/img/1.jpg"></a>
-					</li>
-					<li>
-						<a class="ns-img" href="{{url('/')}}/test/slideshow/img/2.jpg"></a>
-					</li>
-					<li>
-						<a class="ns-img" href="{{url('/')}}/test/slideshow/img/3.jpg"></a>
-					</li>
-					<li>
-						<a class="ns-img" href="{{url('/')}}/test/slideshow/img/4.jpg"></a>
-					</li>
-					<li>
-						<a class="ns-img" href="{{url('/')}}/test/slideshow/img/5.jpg"></a>
-					</li>
+					@foreach ($tempImages as $tempImage)
+						<li>
+							<a class="ns-img" href="{{ $tempImage }}"></a>
+						</li>
+					@endforeach
 				</ul>
 				<div class="navsWrapper">
 					<div id="ninja-slider-prev"></div>
