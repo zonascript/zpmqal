e483bc02a04032499d10d6ef876ed38976c3528b
Ajay Kumar <ajay@flygoldfinch.com>
2017-07-22 20:46:11 +0530

    improved

--- resources/views/b2b/emails/package/partials/header_logo.blade.php
+++ resources/views/b2b/emails/package/partials/header_logo.blade.php
@@ -1,12 +1,12 @@
 <table align="center" bgcolor="#f4f4f4" cellpadding="20" cellspacing="0" width="650">
 	<tbody>
 		<tr>
-			<td bgcolor="{{ $theme->color }}">
+			<td bgcolor="{{ $theme->color7 }}">
 				<table align="left" cellpadding="0" cellspacing="0" width="30%">
 					<tbody>
 						<tr>
 							<td>
-								<a> <img align="left" alt="logo" border="0" src="http://storage.flygoldfinch.dev/storage/images/icon/FGFLOGO4.png" style="display:block" class="CToWUd">
+								<a> <img align="left" alt="logo" border="0" height="50" src="http://storage.flygoldfinch.dev/storage/images/package/icon/logo.png" style="display:block" class="CToWUd">
 								</a>
 							</td>
 						</tr>
