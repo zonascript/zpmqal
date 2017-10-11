93f0afca227b6679e4f7c3139c46ba073b3f72f8
Ajay Kumar <ajay.expert@live.com>
2017-10-12 03:17:34 +0530

    temporary fix

--- app/Http/Controllers/HotelApp/AgodaHotelRoomsController.php
+++ app/Http/Controllers/HotelApp/AgodaHotelRoomsController.php
@@ -145,27 +145,31 @@ class AgodaHotelRoomsController extends Controller
 	public function checkIsMoved()
 	{
 		include_once app_path('MyLibrary/simple_html_dom.php');
+		$result = false;
 
 		$html = file_get_html($this->path);
-		$titleObj = $html->find('title', 0);
-		$title = isset($titleObj->plaintext) ? $titleObj->plaintext : '';
-		$roomCol = $html->find('td[class=room_col]',0);
+		
+		if (is_array($html) || is_object($html)) {
 
-		$result = false;
-		if (findWord('Object moved', $title) && $this->attmpt) {
-			$newUrlObj = $html->find('a', 0);
-			$this->url = $newUrlObj->href;
-			$this->agodaHotel->old_url = $this->agodaHotel->old_url;
-			$this->agodaHotel->url = $this->url;
-			$this->agodaHotel->save();
-			$result = true;
-			rename($this->path, $this->trashPath());
-			$this->path = null;
-			$this->attmpt = $this->attmpt-1;
-		}
-		elseif (is_null($roomCol) && $this->attmpt) {
-			$result = true;
-			$this->attmpt = $this->attmpt-1;
+			$titleObj = $html->find('title', 0);
+			$title = isset($titleObj->plaintext) ? $titleObj->plaintext : '';
+			$roomCol = $html->find('td[class=room_col]',0);
+
+			if (findWord('Object moved', $title) && $this->attmpt) {
+				$newUrlObj = $html->find('a', 0);
+				$this->url = $newUrlObj->href;
+				$this->agodaHotel->old_url = $this->agodaHotel->old_url;
+				$this->agodaHotel->url = $this->url;
+				$this->agodaHotel->save();
+				$result = true;
+				rename($this->path, $this->trashPath());
+				$this->path = null;
+				$this->attmpt = $this->attmpt-1;
+			}
+			elseif (is_null($roomCol) && $this->attmpt) {
+				$result = true;
+				$this->attmpt = $this->attmpt-1;
+			}
 		}
 
 		return $result;
