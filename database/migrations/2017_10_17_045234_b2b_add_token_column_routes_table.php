5795a42ee77583d7d3f992e3223521a7d2083378
Ajay Kumar <ajay.expert@live.com>
2017-10-20 15:20:07 +0530

    added token col in route

--- database/migrations/2017_10_17_045234_b2b_add_token_column_routes_table.php
+++ database/migrations/2017_10_17_045234_b2b_add_token_column_routes_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Models\B2bApp\RouteModel;
 
 class B2bAddTokenColumnRoutesTable extends Migration
 {
@@ -14,7 +15,21 @@ class B2bAddTokenColumnRoutesTable extends Migration
     public function up()
     {
         Schema::table('routes', function (Blueprint $table) {
-            $table->string('token', 50)->after('id')->unique();
+            $table->string('token', 50)->after('id');
+        });
+        
+        $lastRec = RouteModel::orderBy('id', 'desc')->first();
+        $query = [];
+        if (!is_null($lastRec)) {
+            for ($i=1; $i <= $lastRec->id; $i++) { 
+                $query[] = "UPDATE `trawish_b2b`.`routes` SET `token`='".new_token()."' WHERE `id` = '".$i."';";
+            }
+        }
+        $query = implode('', $query);
+        \DB::unprepared($query);
+
+        Schema::table('routes', function (Blueprint $table) {
+            $table->string('token', 50)->unique()->change();
         });
     }
 
