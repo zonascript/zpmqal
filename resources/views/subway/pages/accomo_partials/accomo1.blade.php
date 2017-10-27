5cbc42c5b183d314aeb930419472aa2ab5e82d5b
Ajay Kumar <ajay.expert@live.com>
2017-10-27 20:29:36 +0530

    update

--- resources/views/subway/pages/accomo_partials/accomo1.blade.php
+++ resources/views/subway/pages/accomo_partials/accomo1.blade.php
@@ -7,34 +7,39 @@
 		</header>
 		
     @foreach ($package->accomoRoutes as $key => $route)
-			<?php
-				$images = $route->fusion->images();
-				$accomo = $route->accomo();
-				$images[] = $accomo->image;
-				$images = array_unique($images);
-			?>
 			<div class="content clearfix">
-			  <div class="width-30-p height-200px pull-left m-right-10">
-					<div class="gi-carousel-main">
-				    <div class="GICarousel carousel-box{{$key}} GI_C_wrapper">
-				      <ul class="GI_IC_items" style="{{ $package->accomoRoutes->count() == 1 ? "display: block;" : ''}}">
-								@foreach ($images as $image)
-									<li>
-										<img height="195" width="100%" class="align-left" alt="Hotel Image" src="{{ $image }}" />
-									</li>
-								@endforeach
-				      </ul>
-				    </div>
+			  @if (!is_null($route->accomo()->name))
+					<?php
+						$images = $route->fusion->images();
+						$accomo = $route->accomo();
+						$images[] = $accomo->image;
+						$images = array_unique($images);
+					?>
+				  <div class="width-30-p height-200px pull-left m-right-10">
+						<div class="gi-carousel-main">
+					    <div class="GICarousel carousel-box{{$key}} GI_C_wrapper">
+					      <ul class="GI_IC_items" style="{{ $package->accomoRoutes->count() == 1 ? "display: block;" : ''}}">
+									@foreach ($images as $image)
+										<li>
+											<img height="195" width="100%" class="align-left" alt="Hotel Image" src="{{ $image }}" />
+										</li>
+									@endforeach
+					      </ul>
+					    </div>
+					  </div>
 				  </div>
-			  </div>
-			  <div class="">
-					<h2 class="m-top-5">{{ $accomo->name }} {!! $accomo->starRatingHtml !!} <small>({{$route->mode}})</small></h2>
-					<p>
-						<div>{{ $accomo->startDate }} - {{ $accomo->endDate }}</div>
-						<div>{{ $accomo->location }}</div>
-					</p>
-					<p>{{ $accomo->description }}</p>
-			  </div>
+				  <div class="">
+						<h2 class="m-top-5">{{ $accomo->name }} {!! $accomo->starRatingHtml !!} <small>({{$route->mode}})</small></h2>
+						<p>
+							<div>{{ $accomo->startDate }} - {{ $accomo->endDate }}</div>
+							<div>{{ $accomo->location }}</div>
+						</p>
+						<p>{{ $accomo->description }}</p>
+				  </div>
+				@else
+					<h3>Something is wrong. please contact your agent.</h3>
+				@endif
+
 			</div>
 			<script type="text/javascript">
 				$('.carousel-box{{$key}}').GICarousel({arrows:true,carousel:true});
